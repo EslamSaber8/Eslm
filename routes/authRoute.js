3d4f409741f27a19924ece18f18d5acd5f3744fd
Tankblank <tankblank1@gmail.com>
2024-02-18 19:48:12 +0200

    last Edit

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,18 +1,32 @@
-const express = require("express")
-const { signupValidator, loginValidator } = require("../utils/validators/authValidator")
+const express = require('express');
+const {
+  signupValidator,
+  loginValidator,
+} = require('../utils/validators/authValidator');
 
-const { signup, login, forgotPassword, verifyPassResetCode, resetPassword, allowedTo, signupAs, protect } = require("../services/authService")
+const {
+  signup,
+  login,
+  forgotPassword,
+  verifyPassResetCode,
+  resetPassword,
+  allowedTo,
+  signupAs,
+  protect
+} = require('../services/authService');
 
-const router = express.Router()
+const router = express.Router();
 //  /workshop
-router.post("/signup/:role", signupValidator, signupAs)
-router.post("/login", loginValidator, login)
-router.post("/forgotPassword", forgotPassword)
-router.post("/verifyResetCode", verifyPassResetCode)
-router.put("/resetPassword", resetPassword)
+router.post('/signup/:role', signupValidator, signupAs);
+router.post('/login',loginValidator,login);
+router.post('/forgotPassword', forgotPassword);
+router.post('/verifyResetCode', verifyPassResetCode);
+router.put('/resetPassword', resetPassword);
 
-router.use(protect)
 
-router.post("/signup", allowedTo("superAdmin"), signupValidator, signup)
+router.use(protect); 
 
-module.exports = router
+router.post('/signup',allowedTo('superAdmin'), signupValidator, signup);
+
+
+module.exports = router;
